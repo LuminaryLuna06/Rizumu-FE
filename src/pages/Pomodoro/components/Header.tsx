a39f0ac8396eb81b5316d874fd8082b7123c3426
Luna <maithetranh@gmail.com>
2025-12-06 21:56:51 +0700

    Ghép StreakPopover + Activity modal

--- src/pages/Pomodoro/components/Header.tsx
+++ src/pages/Pomodoro/components/Header.tsx
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 import ResponsiveButton from "@rizumu/components/ResponsiveButton";
 import UserMenu from "@rizumu/components/UserMenu";
-import { IconChartColumn, IconClock, IconMusicCode } from "@tabler/icons-react";
+import {
+  IconChartColumn,
+  IconClock,
+  IconFireHydrant,
+  IconFlame,
+  IconMusicCode,
+} from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
 import RoomPopover from "./RoomPopover";
 import LeaderboardModal from "@rizumu/components/RankingBoard";
+import ActivitiesModal from "@rizumu/components/ActivitiesModal";
+import StreakPopover from "@rizumu/components/StreakPopover";
 
 function Header() {
   const navigate = useNavigate();
   const [opened, setOpened] = useState(false);
+  const [activityOpened, setActivityOpened] = useState(false);
   return (
     <div className="header flex justify-between h-[8vh]">
       {/* Header Left */}
@@ -23,9 +32,11 @@ function Header() {
       </div>
       {/* Header Right */}
       <div className="header-right flex items-center justify-center gap-x-sm">
+        <StreakPopover />
         <ResponsiveButton
           leftSection={<IconClock size={16} />}
           className="font-semibold md:py-sm"
+          onClick={() => setActivityOpened(!activityOpened)}
         >
           0m
         </ResponsiveButton>
@@ -40,6 +51,10 @@ function Header() {
         <UserMenu />
       </div>
       <LeaderboardModal opened={opened} setOpened={setOpened} />
+      <ActivitiesModal
+        opened={activityOpened}
+        onClose={() => setActivityOpened(false)}
+      />
     </div>
   );
 }
