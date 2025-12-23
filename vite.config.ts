56207588a5ec280503e711da153ea669696a62a8
Luna <maithetranh@gmail.com>
2025-12-23 11:36:22 +0700

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,12 @@ export default defineConfig({
   resolve: {
     alias: {
       "@rizumu": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    https: {
+      key: fs.readFileSync("./localhost-key.pem"),
+      cert: fs.readFileSync("./localhost.pem"),
     },
   },
 });
