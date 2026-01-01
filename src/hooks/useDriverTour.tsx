ab1e7a7fdd38e8436e312e8fd2a81742ba48d9bd
Luna <maithetranh@gmail.com>
2026-01-01 22:00:42 +0700

    promax

--- src/hooks/useDriverTour.tsx
+++ src/hooks/useDriverTour.tsx
@@ -6,6 +6,7 @@ export const useDriverTour = () => {
     const driverObj = driver({
       showProgress: true,
       showButtons: ["next", "previous", "close"],
+      overlayClickBehavior: "nextStep",
       steps: [
         {
           popover: {
